d39494628e730abb0b226a9e74eec1b743a57518
Drac Zihper <draczihper@gmail.com>
2024-06-19 12:27:00 +0300

    Add html webpack plugin

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,11 +1,19 @@
 const path = require('path');
-
+const htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './src/index.html',
+      title: "Webpack + html tutorial",
+      filename: "index.html",
+      inject: "body",
+    }),
+    ],
   module: {
     rules: [
     {
@@ -16,6 +24,18 @@ module.exports = {
       test: /\.(png|svg|jpg|jpeg|gif)$/i,
       type: 'asset/resource',
     },
+     {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+       {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
     ],
   },
 };
